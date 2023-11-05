19c204b6a2b01cd3784b4439e66834e5d07a2743
yamagame <yamagame@mac.com>
2023-11-05 22:34:40 +0900

    ラズパイゲームパッド検討コード追加

--- scripts/rpi-gamepad/setup-gamepad.sh
+++ scripts/rpi-gamepad/setup-gamepad.sh
@@ -7,4 +7,7 @@ cd `dirname $0`
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install libusb-1.0-0 libusb-1.0-0-dev libudev-dev
-npm install usb-detection node-hid
+# npm install usb-detection node-hid
+
+# https://wisteriahill.sakura.ne.jp/CMS/WordPress/2021/11/11/raspberry-pi-4-b-ps4-controller/
+# http://www.kernel.org/doc/Documentation/input/joystick-api.txt
